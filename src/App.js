83441ac9f05de0f54ca1beb1d137ebca52a2c779
dev5 <reggiestain@gmail.com>
2020-08-19 20:19:17 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
+import Image1 from "./img/image1.jpg";
+import Image2 from "./img/image2.jpg";
+import Image3 from "./img/image3.jpg";
+import Image4 from "./img/image4.jpg";
+import Image5 from "./img/image5.jpg";
 
 function App() {
+  
+    //Images array 
+    const images = [
+      {
+        "id": 1,
+        "title": "Image 1",
+        "src": Image1,
+      },
+      {
+        "id": 2,
+        "title": "Image 2",
+        "src": Image2,
+      },
+      {
+        "id": 3,
+        "title": "Image 3",
+        "src": Image3,
+      },
+      {
+        "id": 4,
+        "title": "Image 4",
+        "src": Image4,
+      },
+      {
+        "id": 5,
+        "title": "Image 5",
+        "src": Image5,
+      }
+
+   ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <AliceCarousel autoPlay={true} autoPlayInterval="2000">
+     {images.map((image) =>
+     <img key={image.id} src={image.src} className="sliderimg" alt={image.title} />
+    )}
+     </AliceCarousel>
     </div>
   );
 }
